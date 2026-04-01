56bf32e45982f732bfb380843e76264f3676d86b
RoboticReaper <liubaoren2006@gmail.com>
2026-03-31 23:42:47 -0500

    use orangepi for database

--- frontend/app/api/lib/db.ts
+++ frontend/app/api/lib/db.ts
@@ -32,43 +32,77 @@ const getPool = async () => {
             connectionTimeoutMillis: 2000,
         });
     } else {
+        // PRODUCTION (ORANGEPI)
+        const ca = process.env.DB_CA_BASE64;
+        const cert = process.env.DB_CERT_BASE64
+        const key = process.env.DB_KEY_BASE64
+        if (!ca) {
+            throw new Error("Missing DB_CA_BASE64");
+        }
+        if (!cert) {
+            throw new Error("Missing DB_CERT_BASE64");
+        }
+        if (!key) {
+            throw new Error("Missing DB_KEY_BASE64");
+        }
+        const sslConfig = {
+            rejectUnauthorized: true,
+            ca: Buffer.from(ca, 'base64').toString('utf-8'),
+            cert: Buffer.from(cert, 'base64').toString('utf-8'),
+            key: Buffer.from(key, 'base64').toString('utf-8'),
+        }
+
+        const db_url = process.env.DATABASE_URL
+        if (!db_url) {
+            throw new Error("Missing DATABASE_URL");
+        }
+
+        pool = new Pool({
+            connectionString: db_url,
+            ssl: sslConfig,
+            max: 24,
+        })
+
+
+
         // =====================
         // PRODUCTION (VERCEL)
         // =====================
-        if (!process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
-            throw new Error("Missing GOOGLE_APPLICATION_CREDENTIALS_JSON");
-        }
-        if (!process.env.INSTANCE_CONNECTION_NAME) {
-            throw new Error("Missing INSTANCE_CONNECTION_NAME");
-        }
+        // if (!process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+        //     throw new Error("Missing GOOGLE_APPLICATION_CREDENTIALS_JSON");
+        // }
+        // if (!process.env.INSTANCE_CONNECTION_NAME) {
+        //     throw new Error("Missing INSTANCE_CONNECTION_NAME");
+        // }
 
-        // 1. Parse the credentials directly from the environment variable
-        const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+        // // 1. Parse the credentials directly from the environment variable
+        // const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
-        const auth = new GoogleAuth({
-            credentials,
-            // REQUIRED: The connector needs this specific scope to find your DB IP
-            scopes: ['https://www.googleapis.com/auth/sqlservice.admin'],
-            // REQUIRED: Explicitly pass the project ID from the JSON
-            projectId: credentials.project_id,
-        });
+        // const auth = new GoogleAuth({
+        //     credentials,
+        //     // REQUIRED: The connector needs this specific scope to find your DB IP
+        //     scopes: ['https://www.googleapis.com/auth/sqlservice.admin'],
+        //     // REQUIRED: Explicitly pass the project ID from the JSON
+        //     projectId: credentials.project_id,
+        // });
 
-        // 3. Pass the auth instance to the Connector
-        const connector = new Connector({ auth });
+        // // 3. Pass the auth instance to the Connector
+        // const connector = new Connector({ auth });
 
-        const clientOpts = await connector.getOptions({
-            instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
-            ipType: IpAddressTypes.PUBLIC,
-        });
+        // const clientOpts = await connector.getOptions({
+        //     instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
+        //     ipType: IpAddressTypes.PUBLIC,
+        // });
 
-        pool = new Pool({
-            ...clientOpts,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            max: 24,
-        });
+        // pool = new Pool({
+        //     ...clientOpts,
+        //     user: process.env.DB_USER,
+        //     password: process.env.DB_PASSWORD,
+        //     database: process.env.DB_NAME,
+        //     max: 24,
+        // });
     }
+
 
     global.pgPool = pool;
     return pool;
