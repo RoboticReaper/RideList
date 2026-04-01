e18933d49d44c9d959f8f5ad9f762db5363a0880
RoboticReaper <liubaoren2006@gmail.com>
2026-03-31 23:46:18 -0500

    db name fix

--- frontend/app/api/lib/db.ts
+++ frontend/app/api/lib/db.ts
@@ -60,6 +60,7 @@ const getPool = async () => {
         pool = new Pool({
             connectionString: db_url,
             ssl: sslConfig,
+            database: process.env.DB_NAME,
             max: 24,
         })
 
